8abe88595026f92ebbcaed28771b47c752bb9503
NikaGamkrelidze21 <nika.gamkrelidze.21@btu.edu.ge>
2023-04-11 12:29:48 +0400

    continue of tasks

--- Week4/Task1.py
+++ Week4/Task1.py
@@ -17,12 +17,7 @@ def find_shakespeare_or_rowling():
     return data.fetchall()
 
 def price_condition_lower_then(price):
-    data = cur.execute("""select title from books
-                       where price < :pr""",{"pr":price})
-    return data.fetchall()
-
-def unique_authors():
-    data = cur.execute("""select distinct author from books""")
+    data = cur.execute("""select title from books where price < :pr""",{"pr":price})
     return data.fetchall()
 
 def unique_authors():
@@ -33,7 +28,29 @@ def users_have_money_more_then(money):
     data = cur.execute("""select username from users
                        where balance > :money""", {"money":money})
     return data.fetchall()
+
+def users_have_bought_book():
+    data = cur.execute("""select username from users
+                       where id in (select distinct user_id from purchase)""")
+    return data.fetchall()
     
+def book_ever_sold():
+    data = cur.execute("""select * from books
+                       where id in (select distinct book_id from purchase)""")
+    return data.fetchall()
+    
+def book_never_sold():
+    data = cur.execute("""select title from books
+                       where id not in (select distinct book_id from purchase)""")
+    return data.fetchall()
+
+def users_and_books_they_bought():
+    data = cur.execute("""select u.username, p.book_id 
+                       from users as u inner join purchase p 
+                       on u.id = p.user_id """)
+    return data.fetchall()
+    
+
     
     
     
@@ -51,7 +68,8 @@ test_all(
         price_condition_lower_then("20"),
         unique_authors(),
         users_have_money_more_then(100),
+        users_have_bought_book(),
+        book_ever_sold(),
+        book_never_sold(),
+        users_and_books_they_bought(),
         )
-# task N2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-# task N1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
